364cd12d59144a311f3dfe7c61cee9010f8b8cce
altayburakhan <burakhan.altay@gmail.com>
2025-02-05 10:10:35 +0300

    Chunkıng

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
+chunk_size = 100
+
+for chunk in pd.read_csv(
+    "Sources/video_games.csv", chunksize = chunk_size, dtype={
+            "Title": "category",
+            "Metrics.Review Score" : "int16",
+            "Length.All PlayStyles.Average" : "float16"
+        }
+):
+    print("CHUNK : ",chunk.info(memory_usage="deep"))
 
 df = pd.read_csv("Sources/video_games.csv")
 print(df.info(memory_usage="deep"))
-
 
 # Optimized version
 
