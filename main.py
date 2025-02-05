5d69ea7d33e31566bf16345160a91b9501027d55
altayburakhan <burakhan.altay@gmail.com>
2025-02-05 09:59:43 +0300

    Data Type Transformation

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv("Sources/video_games.csv")
 
-print(df.head())
-print(df.info())
-print(df.describe())
+df = pd.read_csv("Sources/video_games.csv")
+print(df.info(memory_usage="deep"))
+
+
+# Optimized version
+
+df = pd.read_csv("Sources/video_games.csv",
+                 dtype={
+                     "Title": "category",
+                     "Metrics.Review Score" : "int16",
+                     "Length.All PlayStyles.Average" : "float16"
+                 })
+
+print(df.info(memory_usage="deep"))
+
 
